db13c691ecef8a11b11f876eddda7363ea2eea52
pjt <swedf123@gmail.com>
2025-03-20 18:50:59 +0900

    head

--- frontend/components/headFoot/HeaderSub.tsx
+++ frontend/components/headFoot/HeaderSub.tsx
@@ -31,22 +31,21 @@ const HeaderSub = () => {
     };
   }, [isLangMenuOpen]);
   return (
-    <header className="bg-[#332f2d]">
-      <div className="flex w-full max-w-[1200px] text-[#EDECEA] mx-auto py-[25px] justify-between relative items-center">
-        <div className="w-[90px]"></div>
-
+    <header className="absolute w-full h-[200px] bg-[#332f2d] z-30">
+      <div className=" flex w-full h-full text-[#EDECEA] py-[25px] justify-between items-center max-w-[1200px] mx-auto">
         <div
-          className="flex items-center px-[48px] py-0 w-full"
+          className="flex items-center px-[48px] py-0 w-full h-full"
           //   onMouseEnter={onBrandHover}
         >
-          <table className="flex w-full">
+          <table className="flex w-full h-full">
             {HeaderSubData.map((v, i) => (
-              <Link className="flex-1" key={i} href={v.href}>
-                <tr className="flex flex-col">
+              <Link className=" " key={i} href={v.href}>
+                <tr
+                  className="flex flex-col ml-24 w-full h-full"
+                  style={{ border: "1px solid red" }}
+                >
                   {v.list?.map((v) => (
-                    <td className="py-[6px] px-[20px] text-[14px] text-center">
-                      {v}
-                    </td>
+                    <td className=" text-[14px] text-start">{v}</td>
                   ))}
                 </tr>
               </Link>
